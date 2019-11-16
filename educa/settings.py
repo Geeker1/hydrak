490a368d4236417f5b62af30714f3e3ec1b37c06
Ledum <ledumibaakee@gmail.com>
2019-11-16 22:14:34 +0100

    Added procfile an runtime txt and shifted directories

--- educa/settings.py
+++ educa/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'courses',
     'crispy_forms',
